be67b814be06adb26b79624abdeb9c09d90d7ded
Ricardo Lafontant <jrl9298@gmail.com>
2024-07-02 20:03:42 -0400

    .

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,8 @@ class User < ApplicationRecord
     has_secure_password
   
     validates :email, presence: true, uniqueness: true
-    validates :first_name, :last_name, :phone_number, :location, presence: true
+    validates :first_name, :last_name, presence: true
     validates :password, presence: true, length: { minimum: 6 }
+    validates :phone_number, allow_nil:true, allow_blank:true , uniqueness:true
   end
   
