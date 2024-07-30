c881b00ddcee6e28db74e64d40a0f4af7e0c4466
Ricardo Lafontant <jrl9298@gmail.com>
2024-07-30 16:09:05 -0400

    modified validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,13 @@ class User < ApplicationRecord
   
     validates :email, presence: true, uniqueness: true
     validates :first_name, :last_name, presence: true
-    validates :password, presence: true, length: { minimum: 6 }
+    validates :password, presence: true, length: { minimum: 6 }, if: :password_required?
     validates :phone_number, allow_nil:true, allow_blank:true , uniqueness:true
+
+    private
+
+    def password_required?
+      password.present? || new_record?
+    end
   end
   
